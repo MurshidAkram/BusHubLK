3d1fd18a19ce757ae0ea41dd7d62963ea80035bc
Abinaia Kuganesan <abinaiakugan@gmail.com>
2025-07-08 17:46:42 +0530

    Resolved the log in screen opening problem

--- driver-app/src/navigation/AppNavigator.tsx
+++ driver-app/src/navigation/AppNavigator.tsx
@@ -1,14 +1,37 @@
-// AppNavigator.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import TabNavigator from './TabNavigator';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import DriverLoginScreen from "../screens/DriverLoginScreen";
+import HomeScreen from "../screens/HomeScreen";
 
-const AppNavigator = () => {
-    return (
-        <NavigationContainer>
-            <TabNavigator />
-        </NavigationContainer>
-    );
+export type RootStackParamList = {
+  Login: undefined;
+  Home: undefined;
+  Dashboard: undefined;
+  LostAndFound: undefined;
+  Emergency: undefined;
+  Condition: undefined;
+  TravelLog: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const AppNavigator: React.FC = () => {
+  console.log("AppNavigator rendering...");
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={DriverLoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default AppNavigator;
